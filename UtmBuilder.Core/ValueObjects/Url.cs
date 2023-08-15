cae1e93d79d8d2b6badd32836d3774b9dd67f5fc
Igor Cerutti <igor.cpv2@gmail.com>
2023-08-15 16:19:45 -0300

    Adicionado a classe e value objects

---
+++ UtmBuilder.Core/ValueObjects/Url.cs
@@ -0,0 +1,13 @@
+﻿
+namespace UtmBuilder.Core.ValueObjects
+{
+    public class Url : ValueObject
+    {
+        public Url(string adress)
+        {
+            Adress = adress;
+        }
+
+        public string Adress { get; }
+    }
+}
